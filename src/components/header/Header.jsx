fb611ced474afea36456be017a712e374ff2d378
RekaVadasz <vadasz.reka87@gmail.com>
2022-09-20 21:08:57 +0200

    Add styling to Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.css';
+
+//import { NavLink } from 'react-router-dom';
+//import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+export default function Header() {
+    return (
+        <header>
+            <div className='navbar'>
+                <div className='logo'></div>
+                <nav>
+                    <ul>Névválasztó</ul>
+                    <ul>Keresés</ul>
+                    <ul>Beküldés</ul>
+                    <ul>Random</ul>
+                    <ul>Kontakt</ul>
+                </nav>
+            </div>
+            <div className='login-register'>
+                <button className='login-button'>Bejelentkezés</button>
+                <button className='register-button'>Regisztráció</button>
+            </div>
+        </header>
+    )
+}
