8181e7e5cdcdd294c4392aac47f3ae69477389b7
RekaVadasz <vadasz.reka87@gmail.com>
2022-10-04 16:05:13 +0200

    Add Hashlink to Hero section

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 import './Hero.css';
 import Corgi from '../corgi/Corgi';
 
+import { HashLink } from 'react-router-hash-link';
+
+const scrollWithOffset = (el) => {
+  const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
+  const yOffset = -120; 
+  window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
+}
+
 export default function Hero() {
   return (
     <section id='hero-section'>
         <div className='hero-left'>
             <p>Találd meg a tökéletes nevet négylábú kedvencednek!</p>
-            <button>Kipróbálom</button>
+            <HashLink 
+              smooth 
+              scroll={el => scrollWithOffset(el)}
+              to='/home#search-section'
+              ><button>Kipróbálom</button>
+            </HashLink>
+
         </div>
 
         <div className='hero-animation'>
