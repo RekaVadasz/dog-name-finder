5126016b2c00e433d2005eda332169fc7fc39e2f
RekaVadasz <vadasz.reka87@gmail.com>
2022-10-10 18:09:44 +0200

    Complete search function on server

--- backend/server.js
+++ backend/server.js
@@ -27,34 +27,102 @@ app.get('/api/search', (req, res) => {
        
         const size = req.query.size; // kicsi / közepes /nagy 
         const gender = req.query.gender; // lány / fiú
-        const breed = req.query.breed; // mindegy / keverék / ...stb 
-                
-        //-- if no traits are defined: search all dogs of given gender, size and breed ---
+        const breed = req.query.breed; // OPTIONAL - (default: mindegy)  / keverék / tacskó ...stb 
+        const traits = req.query.traits; // OPTIONAL - 0, 1 or more
+        
+        let newDogList = []
 
-        if (req.query.traits == undefined) {
-            const newDogList = dogList.filter((dog) => {
-                if (breed === "mindegy") {
-                    return (
-                        dog.size === size 
-                        && dog.gender === gender  
-                    )
-                } else {
-                    return (
-                        dog.size === size 
-                        && dog.gender === gender 
-                        && dog.breed === breed
-                    )
-                }
 
-            }) 
-            res.send(newDogList)
-        } else {
-            const traits = req.query.traits;
-            res.send("under construction")
+        const filterPhysicalAppearance = function (dog) {
+            if (breed === "mindegy") {
+                return (
+                    dog.size === size 
+                    && dog.gender === gender  
+                )
+            } else {
+                return (
+                    dog.size === size 
+                    && dog.gender === gender 
+                    && dog.breed === breed
+                )
+            }
         }
 
-        //console.log(newDogList)
         
+        //--- if no personality traits are defined: search all dogs based on physical appearance ---
+        if (req.query.traits == undefined) {
+            newDogList = dogList
+                .filter(filterPhysicalAppearance) 
+            
+
+
+        //if at least one trait is defined, search only in those
+        } else {
+            console.log(traits)
+
+            //if only 1 trait is defined (query parameter is treated as string)
+            if (typeof traits === "string") {
+                newDogList = dogList
+                    .filter(dog => {
+                        return dog.traits.includes(traits)
+                        })
+                    .filter(filterPhysicalAppearance)
+                
+                console.log(newDogList)
+            
+            // if more than 1 traits are defined
+            } else {
+
+                // iterate through all traits given
+                for (const trait of traits) {
+                    if (newDogList.length === 0) {
+                        //1st iteration
+                        console.log("newDogList was empty")
+
+                            /*                         
+                            dogList.forEach(dog => {
+                            if (dog.traits.includes(trait)) {
+                                newDogList.push(dog)
+                            }
+                            });
+                            */
+                        newDogList = dogList
+                            .filter(dog => {
+                                return dog.traits.includes(trait)
+                            })
+                            .filter(filterPhysicalAppearance)
+
+                    } else {
+                        // 2nd or more iteration
+                        newDogList = newDogList
+                            .filter(dog => {
+                                return dog.traits.includes(trait)
+                            })
+                        console.log(newDogList)
+                    }
+    
+                }
+                
+
+
+
+            }
+
+
+            
+
+
+        }
+
+        res.send(newDogList)
+
+        //console.log(newDogList)
+     
+        
+
+
+
+
         
     });
 });
