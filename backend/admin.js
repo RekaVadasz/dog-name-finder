6960f55abf92ba3dcb8a56a3c9438469a53623b1
RekaVadasz <vadasz.reka87@gmail.com>
2022-10-24 17:55:29 +0200

    Put Firebase credentials in .env file

--- backend/admin.js
+++ backend/admin.js
@@ -1,6 +1,11 @@
 var admin = require("firebase-admin");
+require('dotenv').config(); //loads any file in .env into an environmental variable
 
-var serviceAccount = require("./doggobase-development-6ff8f0a63a66.json");
+//console.log(process.env)
+
+//var serviceAccount = require("./doggobase-development-6ff8f0a63a66.json");
+
+var serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
